fbfc5704a7a50e1498d89220b7f3a7c673634af5
hryshko-denys <gryshko.denys@gmail.com>
2021-02-22 18:26:03 +0200

    test

--- src/use/fetch.ts
+++ src/use/fetch.ts
@@ -14,6 +14,6 @@ export function useFetch<T>(url: RequestInfo, options?: RequestInit) {
     const res = await fetch(url, options);
     response.value = await res.json();
   };
-  //asdfsadfasdfasdfsadfasdf
+  //1
   return { response, request };
 }
